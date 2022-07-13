333bbca39b6fabff06242abb0184a907fa69534a
Bhavin Parghi <bhavin@atharvasystem.com>
2022-07-13 19:47:15 +0530

    Change Html To PDF content

--- src/screens/HTMLToPDF.tsx
+++ src/screens/HTMLToPDF.tsx
@@ -1,15 +1,16 @@
 import {View, Button} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import RNHTMLtoPDF, {Options} from 'react-native-html-to-pdf';
 import {HTMLToPDFProps} from '../navigationTypes';
 import doGenerateHTML from '../htmls/htmlGenerator';
+import {useFocusEffect} from '@react-navigation/native';
 
 const HTMLToPDF: React.FC<HTMLToPDFProps> = ({navigation}) => {
   const [pdfFile, setPDFFile] = useState('');
 
   const createPDF = async () => {
     let options: Options = {
-      html: doGenerateHTML({title: 'Jitendra'}),
+      html: doGenerateHTML(),
       fileName: 'test',
       directory: 'Documents',
     };
@@ -23,6 +24,16 @@ const HTMLToPDF: React.FC<HTMLToPDFProps> = ({navigation}) => {
   const openPDF = () => {
     navigation.navigate('ViewPDFScreen', {pdfFile: pdfFile});
   };
+  useFocusEffect(
+    useCallback(() => {
+      console.log('Screen was focused');
+      setPDFFile('');
+      return () => {
+        console.log('Screen was unfocused');
+        // Useful for cleanup functions
+      };
+    }, []),
+  );
   return (
     <View>
       <Button title="Create PDF" onPress={createPDF} />
